f30155426b628d10b838995533743bbd366e9e89
Pablo Alexandre Nascimento Salgado <pabloalexandre.nave@gmail.com>
2019-04-14 14:55:00 -0300

    Update Home.js

--- src/modules/Home.js
+++ src/modules/Home.js
@@ -5,11 +5,12 @@ import Imagery from '../imagery';
 import data from '../data';
 
 
-const Search = ({ onChange, value }) => (
+/*const Search = ({ onChange, value }) => (
   <div className="search">
     <input className="search__input" value={value} onChange={onChange} type="text" placeholder="Search from album" />
   </div>
-)
+)*/
+
 const Card = ({ preview, name, id, slug, tracks }) => (
   <div onClick={() => preview(slug)} className="home__album">
     <Imagery name={slug} />
@@ -22,7 +23,7 @@ const Card = ({ preview, name, id, slug, tracks }) => (
 const Home = (props) => {
 
   const [searchField, setSearchField] = useState('');
-  const onChange = (e) => setSearchField(e.target.value);
+  // const onChange = (e) => setSearchField(e.target.value);
   return (
       <div className="home">
         <h1 className="home__title">My Music <span>by Piripak.cc</span></h1>
